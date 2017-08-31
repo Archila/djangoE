d0f45f66c77f20d5f57f8ac0a7dfd6879981901a
Archila <ivanestarchila@gmail.com>
2017-08-31 10:02:50 -0600

    Se creo la primer vista Se implemento una consulta La consulta consiste en retornar todas las publicaciones que tiene fecha de publicacion

--- blog/models.py
+++ blog/models.py
@@ -10,10 +10,26 @@ class Post(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
 
-    def Post(self):
+    def publicacion (self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+        
 # Create your models here.
+class Publicacion(models.Model):
+        autor = models.ForeignKey('auth.User')
+        titulo = models.CharField(max_length=200)
+        texto = models.TextField()
+        fecha_creacion = models.DateTimeField(
+                default=timezone.now)
+        fecha_publicacion = models.DateTimeField(
+                blank=True, null=True)
+
+        def publicar(self):
+            self.fecha_publicacion = timezone.now()
+            self.save()
+
+        def __str__(self):
+            return self.titulo
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.listar_pub),
+        url(r'^$', views.listar_publicacion),
 ]
