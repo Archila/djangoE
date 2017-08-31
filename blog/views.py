48463482ed002c488dff15092dd3e4f14aa5a121
Archila <ivanestarchila@gmail.com>
2017-08-31 10:22:40 -0600

    Se modifico la consulta, con el modelo de la base de datos en ingles ya que al subirla a python anywhere no se logra hacer la migracion para crear el nuevo modelo

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
+from .models import Post
 # Create your views here.
-def listar_publicacion(request):
-    pub = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+#def listar_publicacion(request):
+#    pub = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+#    return render(request, 'blog/listar_publicacion.html', {'pub': pub})
+
+def listar_pub(request):
+    pub = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/listar_publicacion.html', {'pub': pub})
