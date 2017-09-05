d489d80836bc2f5ca93d16ac9e01e5ef3cc4cc50
Ivan Esteban Archila <ivan@debian.midominio.com>
2017-09-05 17:46:33 -0600

    Estilos CSS y boostrap Al hacer click en una de las publicaciones redirecciona a su detalle Nueva vista detalle_publicacion

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicacion
 from .models import Post
@@ -10,3 +10,7 @@ from .models import Post
 def listar_pub(request):
     pub = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/listar_publicacion.html', {'pub': pub})
+
+def detalle_pub(request, pk):
+    p=get_object_or_404(Post,pk= pk)
+    return render(request,'blog/detalle_publicacion.html',{'p':p})
