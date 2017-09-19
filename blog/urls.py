03d8d14e8c207bcb440a3a7682b1dfaff746312e
Ivan Esteban Archila <ivan@debian.midominio.com>
2017-09-19 13:18:49 -0600

    Agregado los formularios, ingreso de publicaciones con formulario y editar una publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.listar_pub),
+        url(r'^$', views.listar_pub, name='index'),
         url(r'^postea/(?P<pk>[0-9]+)/$', views.detalle_pub,name='postea'),
+        url(r'^post/new/$', views.post_new, name='post_new'),
+        url(r'^post/edit/(?P<pk>[0-9]+)/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Publicacion
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 #def listar_publicacion(request):
 #    pub = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -14,3 +15,30 @@ def listar_pub(request):
 def detalle_pub(request, pk):
     p=get_object_or_404(Post,pk= pk)
     return render(request,'blog/detalle_publicacion.html',{'p':p})
+
+def post_new (request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('postea', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request,'blog/post_edit.html',{'form':form})
+
+def post_edit (request, pk):
+    post = get_object_or_404(Post,pk= pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('postea', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+        return render(request,'blog/post_edit.html',{'form':form})
